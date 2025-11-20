1d3c19ff015041c70c8ad2cc973333d2c1c78649
Jimi Olaoya <Jimjatt1999@gmail.com>
2025-11-20 17:55:46 +0900

    fix contact us page

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         privacy: resolve(__dirname, 'privacy.html'),
+        contact: resolve(__dirname, 'contact.html'),
       },
     },
   },
